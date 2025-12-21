ef40dd8415a926ea5c545b0225cc0335cdf07a13
Aditya <shindeaditya0258@gmail.com>
2025-12-21 00:12:07 -0600

    email setup

---
+++ server/config/nodemailer.js
@@ -0,0 +1,12 @@
+import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SENDER_EMAIL,
+    pass: process.env.SENDER_PASSWORD
+  }
+});
+
+export default transporter;
