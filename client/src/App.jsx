fe5e84c3b5b2f87a3bcf45b9f759904907fbc56b
Aditya <shindeaditya0258@gmail.com>
2025-12-23 21:42:32 -0600

    initial files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,24 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login.jsx'
+import EmailVerify from './pages/EmailVerify'
+import ResetPassword from './pages/ResetPassword'
+import { ToastContainer } from 'react-toastify';
 
 function App() {
 
   return (
     <>
-      <h1>Hello World</h1>
+      <ToastContainer />
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/login' element={<Login />}/>
+          <Route path='/email-verify' element={<EmailVerify />}/>
+          <Route path='/reset-password' element={<ResetPassword />}/>
+
+      </Routes>
     </>
   )
 }
