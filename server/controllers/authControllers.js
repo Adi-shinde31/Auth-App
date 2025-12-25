545038bc178d2dd99e32f6d10642aea3d500faec
Aditya <shindeaditya0258@gmail.com>
2025-12-24 18:45:41 -0600

    fix otp issues #07

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -103,7 +103,7 @@ export const login = async (req, res) => {
             httpOnly: true,
             secure: true,
             sameSite: 'none',
-            maxAge: 7 * 24 * 60 * 60 * 1000
+            maxAge: 7 * 24 * 60 * 60 * 1000 
         })
 
         return res.json({
