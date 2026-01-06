cff6eb4f0df5415951491913e080c00e43936f51
Aditya <shindeaditya0258@gmail.com>
2026-01-06 14:32:17 -0600

    deploy backend

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -35,8 +35,8 @@ export const register = async (req, res) => {
         //sending cookies
         res.cookie('token', token, {
             httpOnly: true,
-            secure: true,
-            sameSite: 'none',
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
@@ -104,8 +104,8 @@ export const login = async (req, res) => {
         //sending cookies
         res.cookie('token', token, {
             httpOnly: true,
-            secure: true,
-            sameSite: 'none',
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
             maxAge: 7 * 24 * 60 * 60 * 1000 
         })
 
