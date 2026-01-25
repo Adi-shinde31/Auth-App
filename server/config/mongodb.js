c81f7bfc9c1001c1df2c2de519c198f8c801142b
Aditya <shindeaditya0258@gmail.com>
2026-01-24 21:41:58 -0600

    update: app backend

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,17 +1,19 @@
-import mongoose from 'mongoose'
+import mongoose from 'mongoose';
+
+let isConnected = false;
 
 const connectDB = async () => {
-  try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
-    console.log('Database Connected!');
-  } catch (err) {
-    console.error('DB Connection Error:', err.message);
-    process.exit(1);
+  if (isConnected) {
+    return;
   }
-
-  mongoose.connection.on('disconnected', () => {
-    console.log('Database Disconnected');
-  });
+  try {
+    const db = await mongoose.connect(`${process.env.MONGODB_URI}/mern-auth`);
+    isConnected = db.connections[0].readyState;
+    console.log('MongoDB Connected');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error);
+    throw error;
+  }
 };
 
 export default connectDB;
--- server/server.js
+++ server/server.js
@@ -10,31 +10,27 @@ import { authRouter } from './routes/authRoutes.js';
 import { userRouter } from './routes/userRoutes.js';
 
 const app = express();
+
+// Trust Vercel proxy
 app.set('trust proxy', 1);
 
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
-// Allowed frontend origins
+// Allowed frontend origin
 const allowedOrigin = process.env.ALLOWED_ORIGIN;
 
 // Middleware
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({
-  origin: function(origin, callback) {
-      if (!origin) return callback(null, true);
-
-      if (origin === allowedOrigin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
+app.use(
+  cors({
+    origin: allowedOrigin,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 // Routes
 app.get('/', (req, res) => {
@@ -43,10 +39,8 @@ app.get('/', (req, res) => {
 app.use('/api/auth', authRouter);
 app.use('/api/user', userRouter);
 
-// Connect to MongoDB & Start server
-connectDB()
-.then(() => {
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
-})
+// Connect to MongoDB 
+// DB connection (important: do NOT listen here)
+connectDB();
+
+export default app;
